10f927b50c462e3aa27b46429c8e32e5ee50b637
hostem17 <vjjais17@gmail.com>
2022-05-12 01:05:01 +0530

    * Unfreeze functionality added; *freeTokens() *VestedTokens() *frozenTokens()

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,13 +7,11 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Token2 is ERC20, Ownable {
-
     uint256 constant SEPTEMBER_3 = 0; // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SET This
     struct Vesting {
-        uint256 amount;
-        uint256 initialReleaseTime; // SEP 3 / +block.timeStamp+ 30 days
+        // uint256 amountVested;
+        uint256 nextReleaseTime; // SEP 3 / +block.timeStamp+ 30 days
         uint256 initialReleaseAmount;
-        uint256 nextReleaseTime; // +30 days from every vesting
         uint256 monthlyReleaseAmount;
         uint256 cyclesLeft; // set to 9,  decrease on each vesting
     }
@@ -28,8 +26,13 @@ contract Token2 is ERC20, Ownable {
     mapping(address => Source) private _listedSource;
     mapping(address => uint256) private _freeTokens;
     mapping(address => Vesting[]) private _userVestings;
+    mapping(address => uint256) private _amountVested;
 
-    event AddSource(address _address,uint256 _initialReleasePercentage,uint256 _monthlyReleasePercentage );
+    event AddSource(
+        address _address,
+        uint256 _initialReleasePercentage,
+        uint256 _monthlyReleasePercentage
+    );
 
     constructor(
         string memory _name,
@@ -43,20 +46,31 @@ contract Token2 is ERC20, Ownable {
         // _listedSource[msg.sender] = true;
     }
 
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a <= b ? a : b;
+    }
 
-    function addSource(address _sourceAddress, uint256 _initialReleasePercentage,uint256 _monthlyReleasePercentage)
-        external
-        onlyOwner
-    {
+    function addSource(
+        address _sourceAddress,
+        uint256 _initialReleasePercentage,
+        uint256 _monthlyReleasePercentage
+    ) external onlyOwner {
         require(_sourceAddress != address(0), "Cannot add address 0");
 
         require(_listedSource[_sourceAddress].isSet, "Source Already exists");
 
-        _listedSource[_sourceAddress] = Source(_initialReleasePercentage,_monthlyReleasePercentage,true);
+        _listedSource[_sourceAddress] = Source(
+            _initialReleasePercentage,
+            _monthlyReleasePercentage,
+            true
+        );
 
-        emit AddSource(_sourceAddress, _initialReleasePercentage,_monthlyReleasePercentage);
+        emit AddSource(
+            _sourceAddress,
+            _initialReleasePercentage,
+            _monthlyReleasePercentage
+        );
     }
-
 
     function checkSource(address _sourceAddress) public view returns (bool) {
         return _listedSource[_sourceAddress].isSet;
@@ -68,31 +82,126 @@ contract Token2 is ERC20, Ownable {
         uint256 amount
     ) internal virtual override {
         super._beforeTokenTransfer(from, to, amount);
+        unFreeze(from);
         require(amount >= _freeTokens[from], "Not Enough free tokens");
     }
 
+    function addVesting(
+        address to,
+        uint256 amount,
+        uint256 initialReleaseTime,
+        uint256 initialReleasePercentage,
+        uint256 monthlyReleasePercentage
+    ) private {
+        _userVestings[to].push(
+            Vesting(
+                initialReleaseTime,
+                (amount * initialReleasePercentage) / 100,
+                (amount * monthlyReleasePercentage) / 100,
+                1 + (100 - initialReleasePercentage) / monthlyReleasePercentage
+            )
+        );
+    }
 
-    function addVesting(address to, uint256 amount,uint256 initialReleaseTime,uint256 initialReleasePercentage, uint256 monthlyReleasePercentage) private {
-        _userVestings[to].push(Vesting(amount,initialReleaseTime, (amount*initialReleasePercentage)/100 ,initialReleaseTime + 30 days,(amount*monthlyReleasePercentage)/100 ,1 + (100 - initialReleasePercentage)/monthlyReleasePercentage));
-    }   
+    function deleteVesting(address user, uint256 id) internal {
+        _userVestings[user][id] = _userVestings[user][
+            _userVestings[user].length - 1
+        ];
+        delete _userVestings[user][_userVestings[user].length - 1];
+        // Check this
+    }
 
+    function unFreeze(address user) private {
+        for (uint256 i = 0; i < _userVestings[user].length; i++) {
+            // First unfreeze
+            if (block.timestamp > _userVestings[user][i].nextReleaseTime) {
+                if (_userVestings[user][i].initialReleaseAmount > 0) {
+                    _freeTokens[user] += _userVestings[user][i]
+                        .initialReleaseAmount;
+                    _userVestings[user][i].initialReleaseAmount = 0;
+                    _userVestings[user][i].nextReleaseTime += 30 days;
+                }
 
-    // For Owner to send 
-    function sendFrozen(address to, uint256 amount,uint256 initialReleasePercentage, uint256 monthlyReleasePercentage) public onlyOwner {        
+                uint256 cyclesPassed = (block.timestamp -
+                    _userVestings[user][i].nextReleaseTime) % 30 days;
+
+                uint256 cyclesToBePaid = min(
+                    _userVestings[user][i].cyclesLeft,
+                    cyclesPassed
+                );
+
+                _amountVested[user] +=
+                    cyclesToBePaid *
+                    _userVestings[user][i].monthlyReleaseAmount;
+
+                _freeTokens[user] +=
+                    cyclesToBePaid *
+                    _userVestings[user][i].monthlyReleaseAmount;
+
+                _userVestings[user][i].cyclesLeft -= cyclesToBePaid;
+                _userVestings[user][i].nextReleaseTime += (cyclesPassed *
+                    1 days);
+
+                // Check Delete Vesting
+                if (_userVestings[user][i].cyclesLeft == 0) {
+                    deleteVesting(user, i);
+                    i--;
+                }
+            }
+        }
+    }
+
+    function getFrozenTokens(address user) public returns (uint256) {
+        unFreeze(user);
+        return balanceOf(user) - _freeTokens[user];
+    }
+
+    function getVestingSchedules(address user) public view returns (uint256) {
+        uint256 count = 0;
+        for (uint256 i = 0; i < _userVestings[user].length; i++) {
+            count += _userVestings[user][i].cyclesLeft;
+        }
+        return count;
+    }
+
+    function getFreeTokens(address user) public view returns (uint256) {
+        return _freeTokens[user];
+    }
+
+    function getAmonuntVested(address user) public view returns (uint256) {
+        return _amountVested[user];
+    }
+
+    // For Owner to send
+    function sendFrozen(
+        address to,
+        uint256 amount,
+        uint256 initialReleasePercentage,
+        uint256 monthlyReleasePercentage
+    ) public onlyOwner {
         transfer(to, amount);
-         
+
         //  will also call _afterTokenTransfer ????????????????????????????????????????????????????????????????????????????????????????
-        
+
         _freeTokens[to] -= amount; //  free tokens will be increased by _afterTokenTransfer > this line reverts this.
 
-
-        if(block.timestamp < SEPTEMBER_3){
-            addVesting(to, amount,SEPTEMBER_3, initialReleasePercentage ,monthlyReleasePercentage);
+        if (block.timestamp < SEPTEMBER_3) {
+            addVesting(
+                to,
+                amount,
+                SEPTEMBER_3,
+                initialReleasePercentage,
+                monthlyReleasePercentage
+            );
+        } else {
+            addVesting(
+                to,
+                amount,
+                block.timestamp + 30 days,
+                initialReleasePercentage,
+                monthlyReleasePercentage
+            );
         }
-        else{
-            addVesting(to,amount,block.timestamp + 30 days, initialReleasePercentage ,monthlyReleasePercentage);
-        }
-
     }
 
     //  Transfers from all listed launchpads willl be vested 20% -> 30 days, 10% successive months and free tokens are not updated
@@ -102,9 +211,14 @@ contract Token2 is ERC20, Ownable {
         address to,
         uint256 amount
     ) internal virtual override {
-
         if (_listedSource[from].isSet == true) {
-            addVesting(to, amount, block.timestamp + 30 days,(amount * _listedSource[from]._initialReleasePercentage) / 100 , (amount * _listedSource[from]._monthlyReleasePercentage)/100);
+            addVesting(
+                to,
+                amount,
+                block.timestamp + 30 days,
+                (amount * _listedSource[from]._initialReleasePercentage) / 100,
+                (amount * _listedSource[from]._monthlyReleasePercentage) / 100
+            );
         } else {
             _freeTokens[to] += amount;
         }
@@ -114,8 +228,4 @@ contract Token2 is ERC20, Ownable {
         }
         super._afterTokenTransfer(from, to, amount);
     }
-
-
-
-
 }
